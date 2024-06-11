94e456ffa72a01752b11ac0c0e4783c99d3f2fee
jybium <asaolujtomi@gmail.com>
2024-06-11 11:10:35 +0100

    feat - project bootstrap

---
+++ pages/LandingPage.jsx
@@ -0,0 +1,18 @@
+import Footer from "@/components/reusables/Footer";
+import Header from "@/components/reusables/Header";
+import Main from "@/components/reusables/Main";
+import Navbar from "@/components/reusables/Navbar";
+
+const LandingPage =
+  () => {
+    return (
+      <div>
+        <Navbar />
+        <Header />
+        <Main />
+        <Footer />
+      </div>
+    );
+  };
+
+export default LandingPage;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,46 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: ["class"],
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,jsx}",
+    "./components/**/*.{js,jsx}",
+    "./app/**/*.{js,jsx}",
+    "./src/**/*.{js,jsx}",
   ],
+  prefix: "",
   theme: {
+    colors: {
+      primary: "#2FB3E3",
+      pri1: "#EAF7FC",
+      pri9: "#1C6B88",
+      sec10: "#06212B",
+      darkblue: "#1A6884",
+      background:
+        "linear-gradient(90deg, rgba(47,179,227,1) 0%, rgba(26,104,132,1) 100%",
+    },
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
-  plugins: [],
-}
+  plugins: [require("tailwindcss-animate")],
+};
